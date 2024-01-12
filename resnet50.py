d9f752e1bd5a67d57189d16e088adf2138414ba6
Markus Reichold <reichold.markus@gmx.de>
2024-01-12 14:42:17 +0100

    V4.7: Modified ResNet model

--- resnet50.py
+++ resnet50.py
@@ -4,6 +4,7 @@
 # https://www.tensorflow.org/tutorials/images/transfer_learning
 # https://github.com/ovh/ai-training-examples/blob/main/notebooks/computer-vision/image-classification/tensorflow/resnet50/notebook-resnet-transfer-learning-image-classification.ipynb
 # https://stackabuse.com/dont-use-flatten-global-pooling-for-cnns-with-tensorflow-and-keras/
+# https://www.quora.com/Why-was-global-average-pooling-used-instead-of-a-fully-connected-layer-in-GoogLeNet-and-how-was-it-different
 
 from keras import layers
 from keras.models import Model
@@ -20,6 +21,7 @@ def resnet_model(ds_shape, dropout, num_classes=2):
     )
     # print(resnet_50.summary())
     x = resnet_50.output
+    """
     x = layers.GlobalAveragePooling2D()(x)
     x = layers.Dense(1024, activation='relu', name='dense_0')(x) 
     x = layers.Dropout(dropout)(x)
@@ -27,6 +29,9 @@ def resnet_model(ds_shape, dropout, num_classes=2):
     x = layers.Dropout(dropout)(x)
     x = layers.Dense(128, activation='relu', name='dense_2')(x) 
     x = layers.Dropout(dropout)(x)
+    """
+    x = layers.GlobalAveragePooling2D()(x)   
+    # Classifier:
     predictions = layers.Dense(num_classes, activation='softmax', name='output',)(x)
     model = Model(inputs = resnet_50.input, outputs = predictions)
 
